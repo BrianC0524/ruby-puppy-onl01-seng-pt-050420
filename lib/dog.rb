7cf7d55cfa89ff471f76ffe8ff74219809a9ea4b
Brian Craycraft <craycraftb24@hotmail.com>
2020-05-31 19:35:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
 # Add your code here
+class Dog
+  
+  attr_accessor :name
+  
+   @@all_dogs = []
+  def initialize(name)
+    
+    @@all_dogs.push(self)
+    
+  end
+  
+  def self.all
+    
+    return @@all_dogs
+    
+  end
+  
+  def self.print_all
+    
+    @@all_dogs.each {|dog| puts dog}
+    
+  end
+  
+  
+end
